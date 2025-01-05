2c45cb863d7fd5ff44fa2c391bb902dc3585a7cf
unknown <100k1@naver.com>
2025-01-05 22:35:45 +0900

    리액트 최신 라우팅

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./pages/Main/Main";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+  {
+    path: "/new",
+    element: <div>쿠켓신공 페이지</div>,
+  },
+  {
+    path: "/best",
+    element: <div>베스트 페이지</div>,
+  },
+  {
+    path: "/new-items",
+    element: <div>신상품 페이지</div>,
+  },
+  {
+    path: "/discount",
+    element: <div>할인중 페이지</div>,
+  },
+  {
+    path: "/events",
+    element: <div>이벤트 페이지</div>,
+  },
+]);
+
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/new" element={<div>쿠켓신공 페이지</div>} />
-        <Route path="/best" element={<div>베스트 페이지</div>} />
-        <Route path="/new-items" element={<div>신상품 페이지</div>} />
-        <Route path="/discount" element={<div>할인중 페이지</div>} />
-        <Route path="/events" element={<div>이벤트 페이지</div>} />
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
